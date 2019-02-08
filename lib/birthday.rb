9e1c1149707ec2b70584d99b88ce84acc288a370
Ming Duan <11203duan@gmail.com>
2019-02-08 02:14:07 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,5 +6,7 @@
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
-  
+  birthday_kids.each {
+    |kid, age| puts "Happy Birthday #{kid}! You are now #{age} years old!"
+  }
 end
